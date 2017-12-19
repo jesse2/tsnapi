7492733ad4a0504df989af91b4c1e44c4200aa87
Jesse <jesse9726@gmail.com>
2017-12-18 18:05:31 -0700

    fixed some things

--- app.js
+++ app.js
@@ -2,20 +2,42 @@ var express = require('express')
 var app = express()
 var path = require("path");
 var bodyParser=require('body-parser');
+var request=require('request-promise');
+var cheerio=require('cheerio');
+var cheerio2=require('cheerio');
+var tbscrape=require('table-scraper');
 
-var port=3000;
+var port=8080;
 app.use(express.static(path.join(__dirname, '/public')));
 app.use(bodyParser.json());
-
-var tsn=require('./scraper/scrape');
-app.use('/tsn',tsn);
 
 app.get('/', (req,res)=>{
     res.send('Hello world');
 });
 
-app.get('*', (req,res)=>{
-    res.sendFile(path.join(__dirname,'public/index.html'));
+app.post('/pages',function(req,res){
+    counter:Int32Array=0;
+    var options={
+        method:'GET',
+        url:'http://theshownation.com/marketplace/search?&main_filter=MLB+Cards&min_price='+req.body.buy+'&max_price='+req.body.sell+'&series_id='+req.body.series
+    }
+    request(options).then(function(response){
+        urls=Array();
+        urls.length=0;
+        var $=cheerio.load(response);
+        //first scraping, count number of pages of results
+        var two=$('div[class=moreLinks]').html();
+        var $$=cheerio2.load(two);
+        counter= $$('a').eq(-2).text();
+        for(var i=1;i<=counter;i++){
+            //customize url with i value
+            url2='http://theshownation.com/marketplace/search?page='+i+'&main_filter=MLB+Cards&min_price='+req.body.buy+'&max_price='+req.body.sell+'&series_id='+req.body.series;
+            urls.push(url2);
+        }            
+        res.send(urls);
+    }).catch(function(err){
+        console.log("error in getting pages: "+err);
+    });
 });
 
 
