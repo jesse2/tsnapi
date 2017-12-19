e2448f40e4bae41065c286b469dce5cf7d3beece
Jesse <jesse9726@gmail.com>
2017-12-18 18:52:28 -0700

    should work now

--- app.js
+++ app.js
@@ -6,13 +6,9 @@ var request=require('request-promise');
 var cheerio=require('cheerio');
 var cheerio2=require('cheerio');
 var tbscrape=require('table-scraper');
-
+var cors=require('cors');
 app.use(bodyParser.json());
-
-app.get('/', function(req,res){
-res.send("hello sucka world");
-});
-
+app.use(cors());
 app.post('/pages',function(req,res){
     counter:Int32Array=0;
     var options={
@@ -36,6 +32,69 @@ app.post('/pages',function(req,res){
     }).catch(function(err){
         console.log("error in getting pages: "+err);
     });
+});
+
+app.post('/read',function(req,res){
+    var options={
+        method:'GET',
+        url:req.body.allurls
+    }
+    request(options).then(function(response){
+        urls:Array();
+        urls.length=0;
+        var $=cheerio.load(response);
+        var three=$('table[class=marketplace-search]').html();
+        var $$=cheerio2.load(three);
+        $$('a').each(function(i, elem){
+        var ending=$$(this).attr('href');
+        url3='http://theshownation.com'+ending;
+        urls.push(url3);
+        });
+        res.send(urls);
+    }).catch(function(err){
+        console.log("error in getting player urls: "+err);
+    });
+});
+
+app.post('/player', function(req,res){
+    var options={
+        method:'GET',
+        url:req.body.urls
+    }
+    request(options).then(function(response){
+        var $2=cheerio.load(response);
+        var five=$2('h2').text();
+        var six=five.split('\n');
+        var name=six[3];
+        var player={
+                name:name,
+                url:options.url
+            };
+        res.send(player);
+    }).catch(function(err){
+        console.log("error in getting player name: "+err);
+    });
+});
+
+app.post('/prices', function(req,res){
+    url3=req.body.url;
+    tbscrape.get(url3).then(function(tableData) {
+        var buy=tableData[1][0]['Buy Price'];
+        var sell=tableData[2][0]['Sell Price'];
+        buy=buy.replace(',','');
+        sell=sell.replace(',','');
+        var profit=parseInt(buy)-parseInt(sell);
+        var player={
+            name:req.body.name,
+            url:req.body.url,
+            buy:buy,
+            sell:sell,
+            profit:profit
+        }
+        res.send(player);
+        }).catch(function(err){
+            console.log("error in getting prices: "+err);
+        });
 });
 
 
